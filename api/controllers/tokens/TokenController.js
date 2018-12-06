31ac6770747bd5fa36e8b0e3c7d3051d79baaa27
Jules Dourlens <jdourlens@gmail.com>
2018-12-06 16:52:23 +0400

    View for token listing

--- api/controllers/tokens/TokenController.js
+++ api/controllers/tokens/TokenController.js
@@ -1,5 +1,21 @@
 module.exports = {
 
+  tokens: async function(req, res) {
+    return (res.view('tokens/list', {
+      nav: {
+        name: 'tokens',
+      }
+    }));
+  },
+
+  nfts: async function(req, res) {
+    return (res.view('tokens/list', {
+      nav: {
+        name: 'nfts',
+      }
+    }));
+  },
+
   list: async function(req, res) {
     console.log(req.query)
     var results = await sails.helpers.tokens.list.with(req.query);
