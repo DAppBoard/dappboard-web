5180b83847556ad577c9d02db5cd3e4b3c49c244
Jules Dourlens <jdourlens@gmail.com>
2019-01-24 00:37:25 +0400

    0x dashboard

---
+++ assets/js/components/dapps/zrx/tradingPair.js
@@ -0,0 +1,56 @@
+parasails.registerComponent('zrx_trading_pair', {
+
+  props: [
+    'sym1',
+    'name1',
+    'address1',
+    'sym2',
+    'name2',
+    'address2',
+  ],
+
+  data: function() {
+    return {
+    };
+  },
+
+  template: `
+  <center class="columns">
+    <a class="column">
+      {{name1}}
+    </a>
+    <a class="column">
+    {{name2}}
+    </a>
+    </center>
+  `,
+
+  beforeMount: function() {
+  },
+  mounted: async function() {
+
+  },
+  beforeDestroy: function() {
+
+  },
+
+  filters: {
+    getAddress: function(value) {
+      return ("https://raw.githubusercontent.com/TrustWallet/tokens/master/tokens/0x" + value.toLowerCase() + ".png");
+    },
+    contractAddress: function(value) {
+      return "/contract/" + value + "/token";
+    }
+  },
+  watch: {
+    address: function(val) {
+      this.loaded = false;
+    },
+  },
+  methods: {
+    load: function(a, b) {
+      console.log("load", a, b)
+      this.loaded = true;
+    }
+  }
+});
--- assets/js/pages/dapps/zrx/index.js
+++ assets/js/pages/dapps/zrx/index.js
@@ -2,14 +2,18 @@ parasails.registerPage('page_dapp_zrx', {
 
   data: {
     columns: [
-      'fname',
       'fsym',
       'count_of_trades',
       'count_of_traders',
       'count_of_relayers',
     ],
     options: {
-      
+      filterable: false,
+      perPageValues: [25, 50, 100, 500],
+      perPage: 25,
+      headings: {
+         fsym: 'Token pair',
+       }
     }
   },
 
