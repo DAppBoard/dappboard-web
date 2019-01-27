4f0bb9f47689b29860987f693e347083b299b835
Jules Dourlens <jdourlens@gmail.com>
2019-01-27 16:50:46 +0100

    Bounties submissions graph

--- assets/js/pages/dapps/bounties/index.js
+++ assets/js/pages/dapps/bounties/index.js
@@ -62,8 +62,8 @@ parasails.registerPage('page_dapp_bounties', {
             datasets: [{
                 type: 'line',
                 label: "Bounties completed",
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: '#388e3c',
+                borderColor: '#388e3c',
                 data: results.fullfilment_accepted,
                 fill: false,
               },
@@ -73,6 +73,39 @@ parasails.registerPage('page_dapp_bounties', {
                 borderColor: '#64b5f6',
                 data: results.bounty_activated,
               }
+            ]
+          },
+
+          // Configuration options go here
+          options: opt
+        });
+
+      });
+      $.get('/api/dapp/bounties/daily', function(res) {
+        var results = HELPERS.normalizeTimeSerie(moment().subtract(90, 'days'), moment(), res);
+        console.log(results)
+        var c1 = $('#dapp_bounties_network_daily_submisiions')[0].getContext('2d');
+        var opt = {
+          tooltips: {
+            mode: 'index',
+            intersect: false,
+          },
+        }
+        ctx.c_transfers = new Chart(c1, {
+          // The type of chart we want to create
+          type: 'bar',
+
+          // The data for our dataset
+          data: {
+            labels: results.day,
+            datasets: [{
+                type: 'line',
+                label: "Submissions",
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: results.bounty_fullfilled,
+                fill: false,
+              },
             ]
           },
 
